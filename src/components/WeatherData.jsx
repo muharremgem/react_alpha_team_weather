4f1ce7f639a48ba0e2716a0a0a9fb84a482dbada
muharrem gem <muharremgem28@gmail.com>
2022-10-01 16:40:02 +0300

    weather_main

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const WeatherData = ({ WeatherData }) => {
   return (
-    <div>
-      <div>WeatherData</div>
+    <div className="max-w-screen-md mx-auto">
+      <div className="flex flex-col justify-center items-center mt-10">
+        <div className="flex">
+          <p className="font-semibold ">
+            <span className="text-2xl">{WeatherData.name}</span>,{"   "}
+            <span className="text-xl">{WeatherData.sys.country}</span>
+          </p>
+        </div>
+        <div className="mt-5">
+          <div className="flex flex-col items-center">
+            <p className="text-xl">{WeatherData.weather[0].main}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
