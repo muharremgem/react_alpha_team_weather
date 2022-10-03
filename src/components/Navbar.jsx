6775aba00aecd60902cbcd78f12230f1d0227007
Muharrem GEM <104659843+muharremgem@users.noreply.github.com>
2022-10-03 17:51:14 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="text-center py-3 bg-slate-300 bg-blue-600 text-4xl">
-      Weather App with TailwindCSS
+       Weather App with TailwindCSS
     </div>
   );
 };
